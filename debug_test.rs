97666a92f20e1a57df6364586ff62acc82df9529
Alphab137 <bngame2334@gmail.com>
2025-11-23 03:39:17 +0700

    chore: fix formatting

--- debug_test.rs
+++ debug_test.rs
@@ -5,13 +5,13 @@ fn debug_reputation_issue() {
     let config = ReputationConfig::default();
     let mut manager = ReputationManager::new(config);
     let peer = format!("test_peer_{}", rand::random::<u64>());
-    
+
     println!("Created peer: {}", peer);
-    
+
     // Check if peer exists
     let score_before = manager.get_score(&peer);
     println!("Score before: {:?}", score_before);
-    
+
     // The test expects this to work
     assert_eq!(score_before, Some(50));
 }
